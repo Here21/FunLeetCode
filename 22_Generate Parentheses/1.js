f4bd50f04dc5208b7d6d28309342a820bf23f8ac
Here21 <deletetmr@gmail.com>
2018-08-06 15:24:25 +0800

    # 22. Generate Parentheses backtracking

---
+++ 22_Generate Parentheses/1.js
@@ -0,0 +1,16 @@
+var generateParenthesis = function(n) {
+  const result = [];
+  backtrack('', 0, 0, n, result);
+  return result;
+
+  function backtrack(str, open, close, max, result){
+    if(str.length === max * 2) {
+      result.push(str);
+      return;
+    }
+    if(open < max) backtrack(str + '(', open + 1, close, max, result);
+    if(close < open) backtrack(str + ')', open, close + 1, max, result);
+  }
+};
+
+console.log(generateParenthesis(3))
