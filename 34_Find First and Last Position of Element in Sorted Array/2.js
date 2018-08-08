79916b0296b442ef33d153608f53634972ac98b9
Here21 <deletetmr@gmail.com>
2018-08-08 17:37:08 +0800

    Linear Scan

--- 34_Find First and Last Position of Element in Sorted Array/2.js
+++ 34_Find First and Last Position of Element in Sorted Array/2.js
@@ -1,5 +1,24 @@
 var searchRange = function(nums, target) {
-  
+  const result = [-1, -1];
+
+  for(let i = 0; i < nums.length; i++) {
+    if(nums[i] === target) {
+      result[0] = i;
+      break;
+    }
+  }
+
+  if(!result[0]) {
+    return result;
+  }
+
+  for(let j = nums.length - 1; j > 0; j--) {
+    if(nums[j] === target) {
+      result[1] = j;
+      break;
+    }
+  }
+  return result;
 };
 
-console.log(searchRange([1, 2, 3], 3))
+console.log(searchRange([1, 2, 3, 3, 3], 3))
