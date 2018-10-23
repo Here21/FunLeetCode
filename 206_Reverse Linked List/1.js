1cfb972db11ba2b44bc8b2361798e28bf9a21e7d
Here21 <deletetmr@gmail.com>
2018-10-23 23:01:15 +0800

    Linked List,Iterative

---
+++ 206_Reverse Linked List/1.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+// function ListNode(val) {
+//   this.val = val;
+//   this.next = null;
+// }
+var reverseList = function(head) {
+  let pre = null; 
+  while(head !== null) {
+    let temp = head.next;
+    head.next = pre;
+    pre = head;
+    head = temp;
+  }
+  return pre; 
+};
+
+const head = {
+  val: 1,
+  next: {
+    val: 2,
+    next: {
+      val: 3,
+      next: {
+        val: 4,
+        next: {
+          val: 5,
+          next: null
+        }
+      }
+    }
+  }
+}
+
+console.log(reverseList(head));
---
+++ 206_Reverse Linked List/README.md
@@ -0,0 +1,19 @@
+# 206. Reverse Linked List
+
+Reverse a singly linked list.
+
+**Example:**
+
+```js
+Input: 1->2->3->4->5->NULL
+Output: 5->4->3->2->1->NULL
+```
+
+**Follow up:**
+
+A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+## 解法
+
+### Iterative
+
