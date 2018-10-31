19685770e052104c4db8ea5656621ce577eb5f24
Here21 <deletetmr@gmail.com>
2018-10-31 23:51:01 +0800

    Tree

---
+++ 617_Merge Two Binary Trees/1.js
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} t1
+ * @param {TreeNode} t2
+ * @return {TreeNode}
+ */
+function TreeNode(val) {
+  this.val = val;
+  this.left = this.right = null;
+}
+
+var mergeTrees = function(t1, t2) {
+    if(t1 === null) return t2;
+    if(t2 === null) return t1;
+
+    const root = new TreeNode(t1.val + t2.val);
+    root.left = mergeTrees(t1.left, t2.left);
+    root.right = mergeTrees(t1.right, t2.right);
+    return root;
+};
+
+const t1 = {
+  val: 1,
+  left: {
+    val: 2,
+    left: null,
+    right: null
+  },
+  right: {
+    val: 3,
+    left: null,
+    right: null
+  }
+}
+
+const t2 = {
+  val: 1,
+  left: {
+    val: 2,
+    left: null,
+    right: null
+  },
+  right: {
+    val: 3,
+    left: null,
+    right: null
+  }
+}
+
+console.log(mergeTrees(null, null));
