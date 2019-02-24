a3eef49fbb62489a9d6b2ddf44943f5df7a64a90
Here21 <deletetmr@gmail.com>
2019-02-24 22:23:23 +0800

    代理模式

--- design_patterns/proxy.js
+++ design_patterns/proxy.js
@@ -1,3 +1,20 @@
-// ES6 Proxy
+/**
+ * 代理模式
+ * 特点：代理是为了控制对对象的访问，不让外部直接访问到对象。
+ * 实例：缓存代理，图片懒加载，合并请求
+ * 与ES6中Proxy不同
+ */
 
-const proxy = new Proxy(target, handler);
+<ul id="ul">
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+</ul>
+<script>
+    let ul = document.querySelector('#ul')
+    ul.addEventListener('click', (event) => {
+        console.log(event.target);
+    })
+</script>
