98df479215d3f5f3baab92ff4b80795f8dd9e75a
Here21 <deletetmr@gmail.com>
2019-03-19 23:45:52 +0800

    2

--- review1/2_add_tow_numbers.js
+++ review1/2_add_tow_numbers.js
@@ -22,23 +22,40 @@ function ListNode(val) {
 
 var addTwoNumbers = function(l1, l2) {
   let list = new ListNode(0);
-  const _l1 = l1;
-  const _l2 = l2;
+  let fakerList = list;
+  let _l1 = l1;
+  let _l2 = l2;
   let carry = 0;
 
-  function backtracking(l1, l2) {
-    if (l1 === null && l2 === null) {
-      return list;
+  while (_l1 || _l2) {
+    // 取链表的值
+    let val1 = _l1 ? _l1.val : 0;
+    let val2 = _l2 ? _l2.val : 0;
+
+    // 求该节点的值
+    let value = val1 + val2 + carry;
+    console.log("value ---", value, "--", carry);
+
+    // 判断进位
+    if (value >= 10) {
+      carry = Math.floor(value / 10);
+      value = value % 10;
+    } else {
+      carry = 0;
     }
-    if (l1 === null) {
-      let res = l2.val + carry;
-      carry = Math.floor(res / 10);
-      list = l2.val + carry;
-    }
-    if (l2 === null) {
-      list = l1.val + carry;
-    }
+
+    // 保存值，切换节点
+    fakerList.next = new ListNode(value);
+    fakerList = fakerList.next;
+
+    _l1 = _l1 ? _l1.next : null;
+    _l2 = _l2 ? _l2.next : null;
   }
+  if (carry) {
+    fakerList.next = new ListNode(carry);
+  }
+
+  return list.next;
 };
 
 const l1 = {
@@ -52,15 +69,17 @@ const l1 = {
   }
 };
 
-const l1 = {
+const l2 = {
   val: 5,
   next: {
     val: 6,
     next: {
-      val: 4,
+      val: 7,
       next: null
     }
   }
 };
 //  1 - 3 - 4
 //  2 - 5 - 7
+
+console.log(addTwoNumbers(l1, l2));
