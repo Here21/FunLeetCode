e409c06f7289847b2ed14995c0d5aef3198a1401
Here21 <deletetmr@gmail.com>
2019-04-18 23:16:39 +0800

    5

---
+++ review1/5_Longest Palindromic Substring.js
@@ -0,0 +1,22 @@
+/*
+5. Longest Palindromic Substring
+
+Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+Example 1:
+
+```js
+Input: "babad"
+Output: "bab"
+```
+
+Note: "aba" is also a valid answer.
+Example 2:
+
+```js
+Input: "cbbd"
+Output: "bb"
+```
+*/
+
+var longestPalindrome = function(s) {};
